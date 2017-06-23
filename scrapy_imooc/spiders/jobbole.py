5300891a3f337e96dc5a50963bb41b32151616dd
yanbing2020 <374726241@qq.com>
2017-06-23 15:43:53 +0800

    0623

---
+++ scrapy_imooc/spiders/jobbole.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import re
+import scrapy
+import datetime
+from scrapy.http import Request
+from urllib import parse
+from scrapy.loader import ItemLoader
+import time
+from scrapy_imooc.items import JobBoleArticleItem
+
+
+# noinspection PyMethodMayBeStatic
+class JobboleSpider(scrapy.Spider):
+    name = 'jobbole'
+    allowed_domains = ['python.jobbole.com']
+    start_urls = ['http://python.jobbole.com/all-posts/']
+
+    def parse(self, response):
+        post_nodes = response.css("#archive .floated-thumb .post-thumb a")
+        for post_node in post_nodes:
+            image_url = post_node.css("img::attr(src)").extract_first("")
+            post_url = post_node.css("::attr(href)").extract_first("")
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"front_image_url": image_url}, callback=self.parse_detail)
+
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_url:
+            yield Request(url=parse.urljoin(response.url, next_url), callback=self.parse)
+
+    @staticmethod
+    def parse_detail(response):
+        ArticleItem = JobBoleArticleItem()
+        print(response.text)
+        pass
+        # article_item = JobBoleArticleItem()
+
